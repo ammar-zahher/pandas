6552b1f8cf2ae80d6fa3bf649101d934770a2eac
Ammar Zahher <ammaryasser1938@gmail.com>
2026-04-02 17:02:38 +0300

    Create first try with pandas(Dataframe).py

---
+++ first try with pandas(Dataframe).py
@@ -0,0 +1,8 @@
+import pandas as pd
+salaries=pd.DataFrame({
+    "NAMES":["ammar","sami","ali","ousan"],
+    "AGE":[15,16,16,17],
+    "YEAR":["first","second","second","therd"],
+    "rest":["last year","one year","one year","last year"]
+    })
+print(salaries)
